28d96e6cb4edd370092dbf02f5210074e192d4b0
zhang_jq <1275834973@qq.com>
2024-02-24 11:53:22 +0800

    调整启动类

--- ynld-server/src/main/java/com/ynld/ServerApplication.java
+++ ynld-server/src/main/java/com/ynld/ServerApplication.java
@@ -1,20 +1,38 @@
 package com.ynld;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author zhang_jq
  * @date 2024/2/22
  * @apiNote 系统启动类
  */
+@Slf4j
 @SpringBootApplication
 public class ServerApplication extends SpringBootServletInitializer {
-  public static void main(String[] args) {
-    SpringApplication.run(ServerApplication.class, args);
-    System.out.println("ServerApplication启动成功");
+  public static void main(String[] args) throws UnknownHostException {
+    ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+    ConfigurableEnvironment env = context.getEnvironment();
+    String ip = InetAddress.getLocalHost().getHostAddress();
+    String port = env.getProperty("server.port");
+    String active = env.getProperty("spring.profiles.active");
+    String path = env.getProperty("server.servlet.context-path");
+    log.info("\n----------------------------------------------------------\n\t" +
+            "Application Ynld-Boot is running! Access URLs:\n\t" +
+            "示例程序【" + active + "】环境已启动! 地址如下:\n\t" +
+            "Local: \t\thttp://localhost:" + port + path + "\n\t" +
+            "External: \thttp://" + ip + ':' + port + path + '\n' +
+            "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+            "----------------------------------------------------------");
   }
 
   @Override
