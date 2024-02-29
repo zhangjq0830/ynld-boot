097c0bb5e20514232f61637b760ab1bfd0ff4564
zhang_jq <1275834973@qq.com>
2024-02-29 08:30:16 +0800

    更新系统结构

--- ynld-framework/ynld-spring-boot-starter-web/src/main/java/com/ynld/framework/banner/config/YnldBannerAutoConfiguration.java
+++ ynld-framework/ynld-spring-boot-starter-web/src/main/java/com/ynld/framework/banner/config/YnldBannerAutoConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class YnldBannerAutoConfiguration {
-
   @Bean
   public BannerApplicationRunner bannerApplicationRunner() {
     return new BannerApplicationRunner();
